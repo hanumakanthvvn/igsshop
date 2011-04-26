e2ebe8a2eeddc377765ee10a2b8abe949f76e060
hanumakanthvvn <hanumakanth@indigenius.com>
2011-04-26 14:12:23 +0530

    changes in mydefs

--- config/initializers/mydefs.rb
+++ config/initializers/mydefs.rb
@@ -1,3 +1,3 @@
 Spree::Config.set(:logo => '/images/logo.jpg')
 Spree::Config.set(:admin_interface_logo => '/images/logo.jpg')
-Spree::Config.set(:stylesheets => 'screen,my_theme,shop/jCx-rYxwpeE,shop/Website_1')
+#Spree::Config.set(:stylesheets => 'screen,my_theme,shop/jCx-rYxwpeE,shop/Website_1')
